c48bfde502b193620d44ceeed1a2f5189a3e3f5b
pouryourlove <lqu134@naver.com>
2023-09-03 15:42:08 +0900

    Link component

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,11 +1,28 @@
+import Link from "next/link";
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
-        <header>[header]</header>
+        <header>
+          <ul>
+            <li>
+              <Link href="/">Home</Link>
+            </li>
+            <li>
+              <Link href="/posts">Posts</Link>
+            </li>
+            <li>
+              <Link href="/about">About</Link>
+            </li>
+          </ul>
+        </header>
         <main>{children}</main>
         <footer>[footer]</footer>
       </body>
     </html>
   );
 }
+
+//client-side navigation
+//to make it a singple page application
