d61ec45f5f2df2f9968e80236b356d8edabb0eff
Juyeon Lee <lqu134@naver.com>
2023-11-03 23:10:55 +0900

    reviews.js created

--- app/posts/appearance/page.jsx
+++ app/posts/appearance/page.jsx
@@ -1,27 +1,24 @@
 import Heading from "@/components/Heading";
-import {readFile} from 'node:fs/promises'
-import matter from "gray-matter"
-import  {marked}  from "marked";
+import {getReview} from "@/lib/reviews"
+
+
 
 export default async function Appearance() {
-  const text = await readFile('./content/posts/appearance.md','utf8')
-  const { content,data: {title,date,image} } = matter(text)
-  const html= marked(content,text)
-
+  const review = await getReview('appearance')
 
   
   return (
     <>
-      <Heading>{title}</Heading>
-      <p className="italic pb-2">{date}</p>
+      <Heading>{review.title}</Heading>
+      <p className="italic pb-2">{review.date}</p>
       <img
-        src={image}
+        src={review.image}
         alt=""
         width="640"
         height="360"
         className="mb-2 rounded"
       />
-      <article dangerouslySetInnerHTML={{__html:html}} className="max-w-screen-sm prose prose-slate"/>
+      <article dangerouslySetInnerHTML={{__html:review.body}} className="max-w-screen-sm prose prose-slate"/>
     </>
   );
 }
